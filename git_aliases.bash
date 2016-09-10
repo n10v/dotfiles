1a6cd418d22cd126045ac40ff822ca65b556af2f
bogem <albertnigma@gmail.com>
2016-09-10 11:54:41 +0500

    Add new git aliases

--- git_aliases.bash
+++ git_aliases.bash
@@ -4,3 +4,8 @@ git config --global alias.co commit
 git config --global alias.d diff
 git config --global alias.p push
 git config --global alias.st status
+git config --global alias.change 'codereview change'
+git config --global alias.mail 'codereview mail'
+git config --global alias.pending 'codereview pending'
+git config --global alias.submit 'codereview submit'
+git config --global alias.sync 'codereview sync'
